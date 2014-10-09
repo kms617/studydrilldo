113fbcba5262b2c792d36dcf362d3a6e8841d62b
Kristin <krysia.codes@gmail.com>
2014-10-09 11:28:39 -0400

    user show page updated to separate admin from members

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,5 +2,7 @@ class Admin::UsersController < ApplicationController
 before_filter :authorize!
   def index
     @users = User.order(created_at: :desc)
+    @admins = @users.where(role: "admin")
+    @members = @users.where(role: "member")
   end
 end
