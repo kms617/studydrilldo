5003b49a978832d7af2fdf866cd85ad1bd38f439
Kristin <krysia.codes@gmail.com>
2014-10-09 22:11:47 -0400

    user can upload a profile photo, tests pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name, :username]
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name, :username, :profile_photo]
+    devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name, :username, :profile_photo]
   end
 
   def authorize!
