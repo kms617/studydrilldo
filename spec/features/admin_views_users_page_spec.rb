9c43d79dec2f5f6b677396a2e5caa9ccb606b27a
Kristin <krysia.codes@gmail.com>
2014-10-13 16:32:44 -0400

    devise initializer reconfig

--- spec/features/admin_views_users_page_spec.rb
+++ spec/features/admin_views_users_page_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 feature 'admin can see a list of users' do
 
   let(:admin) { FactoryGirl.create(:user, role: 'admin') }
-  let!(:user) { FactoryGirl.create(:user) }
+  let!(:member) { FactoryGirl.create(:user) }
 
   scenario 'admin visits users index' do
     sign_in_as(admin)
     visit admin_users_path
 
-    expect(page).to have_content(user.email)
+    expect(page).to have_content(member.email)
   end
 
   scenario 'unauthorized users are redirected' do
-    sign_in_as(user)
+    sign_in_as(member)
     visit admin_users_path
 
     expect(page).to have_content("The page you were looking for doesn't exist.")
