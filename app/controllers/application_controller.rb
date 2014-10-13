6bb0efd17d487da459ce497d34a4fcaf7d50a108
Kristin <krysia.codes@gmail.com>
2014-10-13 17:21:41 -0400

    sign up redirect removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,4 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
    user_path(current_user)
   end
-
-  def after_sign_up_path_for(resource)
-    after_sign_in_path_for(resource)
-  end
 end
