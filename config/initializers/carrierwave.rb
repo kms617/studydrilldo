7c1330b4c9b1a27580af90dfe0577550e200d484
Kristin <krysia.codes@gmail.com>
2014-10-14 10:37:58 -0400

    trying settings for carrierwave and AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,12 +4,15 @@ CarrierWave.configure do |config|
       config.enable_processing = false
   else
     config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'] || "",
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] || ""
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'] || "",
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] || "",
+      region: 'us-east-1',
+      host: 's3-website-us-east-1.amazonaws.com',
+      endpoint: 'https://study.drill.do-production.s3-website-us-east-1.amazonaws.com'
     }
-    config.fog_directory  = ENV['S3_BUCKET']
-    config.fog_public     = true
+    config.fog_directory = ENV['S3_BUCKET']
+    config.fog_public = true
     config.storage = :fog
   end
 end
