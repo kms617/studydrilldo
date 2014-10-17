c66cca3f65c2e3b4318c99ce351d73773cfd7210
Kristin <krysia.codes@gmail.com>
2014-10-17 14:39:18 -0400

    ongoing pie chart now displays

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,41 @@
 //= require highcharts/highcharts-more
 //= require_tree .
 
+var pieChart = function(selector, name, data) {
+    new Highcharts.Chart({
+        chart: {
+            renderTo: selector,
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,//null,
+            plotShadow: false
+        },
+        title: {
+            text: name
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.y:.0f} minutes</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.y:.0f} minutes',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Time',
+            data: data
+        }]
+    });
+}
+
 $(document).ready(function() {
 
     // var ongoing_pie_container = document.createElement('div');
@@ -23,104 +58,24 @@ $(document).ready(function() {
     // document.body.appendChild(ongoing_pie_container);
     // debugger
 
+    var $idealPie = $('#ideal_pie');
+    if ($idealPie.length > 0) {
 
-    var ideal_pie_chart = new Highcharts.Chart({
-        chart: {
-            renderTo: 'ideal_pie',
-            plotBackgroundColor: null,
-            plotBorderWidth: 0,//null,
-            plotShadow: false
-        },
-        title: {
-            text: 'Ideal Allocation'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.y:.0f} minutes</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.y:.0f} minutes',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                }
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: 'Time',
-            data: gon.ideal_pie
-        }]
-    });
+        pieChart('ideal_pie', 'Ideal Allocation', gon.ideal_pie);
+    }
 
-    var actual_pie_chart = new Highcharts.Chart({
-        chart: {
-            renderTo: 'actual_pie',
-            plotBackgroundColor: null,
-            plotBorderWidth: 0,//null,
-            plotShadow: false
-        },
-        title: {
-            text: 'My Allocation'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.y:.0f} minutes</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.y:.0f} minutes',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                }
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: 'Time',
-            data: gon.actual_pie
-        }]
-    });
 
-    var ongoing_pie_chart = new Highcharts.Chart({
-        chart: {
-            renderTo: 'ongoing_pie',
-            plotBackgroundColor: null,
-            plotBorderWidth: 0,//null,
-            plotShadow: false
-        },
-        title: {
-            text: 'Current Status'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.y:.0f} minutes</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.y:.0f} minutes',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                }
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: 'Time',
-            data: gon.ongoing_pie
-        }]
-    });
+     var $actualPie = $('#actual_pie');
+    if ($actualPie.length > 0) {
+
+        pieChart('actual_pie', 'My Allocation', gon.actual_pie);
+    }
+
+    var $ongoingPie = $('#ongoing_pie');
+    if ($ongoingPie.length > 0) {
+
+        pieChart('ongoing_pie', 'Progress', gon.ongoing_pie);
+    }
+
 });
 
